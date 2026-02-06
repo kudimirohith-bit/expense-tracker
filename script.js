ac9dc832c7f8cf45e34e4b3fa65a43e553c4c5e3
Rohith <kudimirohith@gmail.com>
2026-02-06 19:38:36 +0530

    initialize expense tracker UI and project structure

---
+++ script.js
@@ -0,0 +1,11 @@
+const addBtn = document.getElementById("addBtn");
+const expenseList = document.getElementById("expenseList");
+const totalEl = document.getElementById("total");
+
+let total = 0;
+
+addBtn.addEventListener("click", addExpense);
+
+function addExpense() {
+    // logic will be added tomorrow
+}
